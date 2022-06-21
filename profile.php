fc5931af27a5416ceadc8b3494c8cc73b74f9efa
Karl F <karlbferraren@gmail.com>
2022-06-20 20:18:27 -0500

    Everything works, no styling

--- profile.php
+++ profile.php
@@ -30,24 +30,69 @@
 
   </section>
   <section class="voteHistory">
-  <!-- <form class="profileVotes" action="includes/signup.inc.php" method="post"> 
-  </form> -->
 
-  <form class="login" action="includes/votehistory.php" method="post">
-    <button type="submit" name="submit">Check History</button>
-    <input id="profileUserValue" type="text" name="username" value="asdf" hidden> 
+  <form class="login" action="" method="post">
+    <input id="profileUserValue" type="text" name="history" value="true" hidden>
+    <input type="submit" value="Check History"> 
   </form>
   <?php
-    require_once 'includes/votehistory.php';
+   
+   include_once 'includes/dbh.inc.php';
+
+    if(isset($_POST['history'])){
+      
+      if($_POST['history'] == "true"){
+   
+        $userIdentification = $_SESSION["userid"];
+        $sql = "SELECT * FROM RESULTS WHERE user_id = ?;"; // prepared sql statement to retrieve all information from results table where signed in user is saved
+        //echo $sql;
+        $stmt = mysqli_stmt_init($conn);
+
+        if (!mysqli_stmt_prepare($stmt, $sql)){
+          header('location: profile.php?error=insertstmtfailed');
+          exit();
+        }
+        mysqli_stmt_bind_param($stmt, "s", $userIdentification);
+        mysqli_stmt_execute($stmt);
+        $result = mysqli_stmt_get_result($stmt);
+
+        echo "<table>
+                <tr>
+                  <th>Character One</th>
+                  <th>Character Two</th>
+                  <th>Winner</th>
+                </tr>";
+        if (mysqli_num_rows($result) > 0) {
+          while ($row = mysqli_fetch_assoc($result)){
+            // header ('location: profile.php?status=historyFound');
+            $character_one = $row["character_one"];
+            $character_two = $row["character_two"];
+            $winner = $row["winner"] === 0 ? $character_one : $character_two;
+            echo "<tr>
+                      <td>$character_one</td>
+                      <td>$character_two</td>
+                      <td>$winner</td>
+                    </tr>";
+          }
+        }else{
+          echo "<tr>
+                    <td>None</td>
+                    <td>None</td>
+                    <td>None</td>
+                  </tr>";
+        }
+        echo "</table>";
+      }
+    }
   ?>
 
   </section>
 
-  <script>
+  <!-- <script>
       var strUser = document.getElementById("hiddenUsername").textContent;
       console.log(strUser);
       document.getElementById("profileUserValue").value = strUser; 
-  </script>  
+  </script>   -->
 <?php
   require_once 'footer.php';
   // include_once 'xampp\htdocs\meepmerp\meepmerp\header.php';
